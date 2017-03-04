665f203ca87a36d8780322fe45f4858de0a7dea8
Rakesh Lawaju <codearryaas@gmail.com>
2017-03-04 11:25:56 +0545

    Plugin header

---
+++ wpbkt-google-map.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name:       WPBKT Google Map
+ * Plugin URI:        https://wpbkt.com
+ * Description:       WordPress Google Map created by WordPress Bhaktapur Group.
+ * Version:           1.0.0
+ * Author:												WordPress Bhaktapur
+ * Author URI:								https://wpbkt.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:							wpbkt-google-map
+ * Domain Path:       /languages
+ *
+ * @package wpbkt-google-map
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}

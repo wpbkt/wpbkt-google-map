a38f66f5d1232f308d60d3e19fa6523227881ab4
Rakesh Lawaju <codearryaas@gmail.com>
2017-03-04 11:41:56 +0545

    Add menu

--- wpbkt-google-map.php
+++ wpbkt-google-map.php
@@ -18,3 +18,27 @@
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
+
+
+/**
+ * Register a custom menu page.
+ */
+function wpbkt_google_map_add_menu(){
+    add_menu_page(
+        __( 'WPBKT Google Map', 'textdomain' ),
+        'Google Map',
+        'manage_options',
+        'google-map',
+        'wpbkt_google_map_add_menu_callback',
+        'dashicons-marker',
+        6
+    );
+}
+add_action( 'admin_menu', 'wpbkt_google_map_add_menu' );
+
+/**
+ * Display a custom menu page
+ */
+function wpbkt_google_map_add_menu_callback(){
+    esc_html_e( 'Admin Page Test', 'textdomain' );
+}
